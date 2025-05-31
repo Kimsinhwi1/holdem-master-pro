08f521307404b5ec61c563b552550522eb9be80f
Kimsinhwi1 <pok_hera@naver.com>
2025-05-31 18:53:23 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ProPokerMaster from './ProPokerMaster.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import ProPokerMaster from './ProPokerMaster.jsx';
+import './index.css';
+
+// SEO 최적화된 앱 래퍼
+const App = () => {
+  return (
+    <HelmetProvider>
+      <ProPokerMaster />
+    </HelmetProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProPokerMaster />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
