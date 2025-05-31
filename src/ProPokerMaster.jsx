4465a929e27b5dabd831b164eeb00d37b83df0c7
Holdem Master Pro <poker-master-pro@holdem-master.com>
2025-06-01 00:15:08 +0900

    Update ProPokerMaster.jsx

--- src/ProPokerMaster.jsx
+++ src/ProPokerMaster.jsx
@@ -11,10 +11,10 @@ import Announcement from './components/Announcement.jsx';
 
 // 🚀 새로운 SEO 컴포넌트들 (패키지 설치 후 주석 해제)
 import SEOHead from './components/SEOHead.jsx';
-import Navigation from './components/Navigation.jsx';
-import BlogSection from './components/BlogSection.jsx';
-import FAQ from './components/FAQ.jsx';
-import PokerGlossary from './components/PokerGlossary.jsx';
+// import Navigation from './components/Navigation.jsx';
+// import BlogSection from './components/BlogSection.jsx';
+// import FAQ from './components/FAQ.jsx';
+// import PokerGlossary from './components/PokerGlossary.jsx';
 
 // 🎯 임시 SEO Head 컴포넌트 (react-helmet-async 없이도 작동)
 const TempSEOHead = ({ title, description }) => {
